a471f9bf4fcb8a5e70fc16503ae80dfd1ddef357
xuestudy <1104428690@qq.com>
2020-05-21 23:15:06 +0800

    mybatis测试查询数据成功

--- src/main/java/com/essm/controller/TestController.java
+++ src/main/java/com/essm/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.essm.controller;
 
+import com.essm.dao.UserMapper;
+import com.essm.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Description
@@ -11,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Controller
 public class TestController {
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping(value = "/aa")
     public String test() {
         return "index";
     }
+
+    @ResponseBody
+    @GetMapping(value = "/dept/{id}")
+    public User selectById(@PathVariable Integer id) {
+        return userMapper.selectById(id);
+    }
+
 }
