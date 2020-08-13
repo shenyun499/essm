e4a03a547728f4436d9c3471671e4a210e828e77
huangzhixue <1104428690@qq.com>
2020-08-13 19:38:16 +0800

    初始版本项目，让各个小功能支撑起来做的准备

--- src/main/java/com/essm/EssmApplication.java
+++ src/main/java/com/essm/EssmApplication.java
@@ -3,8 +3,10 @@ package com.essm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@MapperScan(basePackages = "com.essm.dao")
+@MapperScan(basePackages = "com.essm")
 @SpringBootApplication
 public class EssmApplication {
 
